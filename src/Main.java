5eb5a6a1bb24fe07ad7582c39aec77dcbe6d4a1c
Haydenj213 <johns3hm@mail.uc.edu>
2025-02-09 16:02:30 -0500

    Second batch of code

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-
         System.out.println("Please enter your birth month as an integer (1-12)");
 
         int birthMonth = in.nextInt();
         in.nextLine();
 
-        if (birthMonth = 1) {
-            System.out.println("Your birth month is Janu");
+        if (birthMonth == 1) {
+            System.out.println("Your birth month is January");
+        } else if (birthMonth == 2) {
+            System.out.println("Your birth month is February");
         }
-
 
 
     }
