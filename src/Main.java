d3352d8541529739ef396706f26b9b417b2a5dad
Haydenj213 <johns3hm@mail.uc.edu>
2025-02-09 16:00:14 -0500

    First batch of code

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println("Please enter your birth month as an integer (1-12)");
+
+        int birthMonth = in.nextInt();
+        in.nextLine();
+
+        if (birthMonth = 1) {
+            System.out.println("Your birth month is Janu");
+        }
+
+
+
+    }
+}
